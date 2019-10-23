7795f10ffe20e93d31b00f90fc0e1a2cb7a04bb3
branislavhesko <branislav.hesko@protonmail.ch>
2019-10-23 21:21:21 +0200

    traffic sign dataset

---
+++ utils/dataset_traffic_signs.py
@@ -0,0 +1,45 @@
+from enum import auto, Enum
+import glob
+import numpy as np
+import os
+from PIL import Image
+import torch
+
+
+class DatasetKeys:
+    LABEL = auto()
+    IMAGE = auto()
+
+
+class TrafficSignDataset(torch.utils.data.Dataset):
+    EXTENSION = "png"
+
+    def __init__(self, base_path: str, num_classes=2, transform=lambda x: x):
+        super().__init__()
+        self._base_path = base_path
+        self._num_classes = num_classes
+        self._dataset = []
+        self._label_map = {}
+        self._transform = transform
+
+    def load(self):
+        folders = next(os.walk(self._base_path))[1]
+        assert self._num_classes == len(folders)
+        dataset = {}
+        for folder in folders:
+            dataset[folder] = glob.glob(os.path.join(self._base_path, folder, "*." + self.EXTENSION))
+        for idx, class_ in enumerate(dataset.keys()):
+            self._dataset.append({
+                DatasetKeys.LABEL: class_,
+                DatasetKeys.IMAGE: dataset[class_]
+            })
+            self._label_map[class_] = idx
+
+    def __len__(self):
+        return len(self._dataset)
+
+    def __getitem__(self, index):
+        label = torch.Tensor([self._dataset[index][DatasetKeys.LABEL]]).long()
+        image = np.array(Image.open(self._dataset[index][DatasetKeys.IMAGE]))
+        image = self._transform(image)
+        return torch.from_numpy(image).permute([2, 0, 1]), label
